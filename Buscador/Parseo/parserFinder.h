a0cc5718b1adc50255ac85cd5d8eee48676a3d1b
martine9111@gmail.com <martine9111@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-04-01 20:21:50 +0000

    Agregado comentarios en un lugar. Sacada una tilde (se puede cargar mal)

--- Buscador/Parseo/parserFinder.h
+++ Buscador/Parseo/parserFinder.h
@@ -5,11 +5,12 @@
 #define PARSERFINDER_ERROR 1
 
 
-/*A partir de argv obtiene los dos parametros que se necesitan obtener.
-En caso de haber mas, se devuelve error (cte PARSERFINDER_ERROR), sino, en 'cadenas'
-quedará guardado en cada lugar (2) el valor de cada uno, siendo el primero el nombre
-del repositorio, y el segundo la consulta a realizar, y devolvera PARSERFINDER_OK.
-Precondicion: 'cadenas' tiene el lugar para (minimamente) de cadenas.*/
+/* A partir de argv obtiene los dos parametros que se necesitan obtener.
+ * En caso de haber mas, se devuelve error (cte PARSERFINDER_ERROR), sino, en 'cadenas'
+ * quedara guardado en cada lugar (2) el valor de cada uno, siendo el primero el nombre
+ * del repositorio, y el segundo la consulta a realizar, y devolvera PARSERFINDER_OK.
+ * Precondicion: 'cadenas' tiene el lugar para (minimamente) de cadenas.
+ * Memoria: Se pide memoria para guardar en ambos lugares (2) de 'cadenas' los parametros requeridos*/
 int parserFinder_obtenerParametros(int argc, char** argv,char** cadenas);
 
 
