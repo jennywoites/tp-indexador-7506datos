3044e222b9939da32dd935c11208c199f755a988
martine9111@gmail.com <martine9111@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-04-23 19:58:30 +0000

    Parseo + Merger terminados (como tienen que estar al final)

--- trunk/Indexador/Manejo de Archivos/registro.h
+++ trunk/Indexador/Manejo de Archivos/registro.h
@@ -4,10 +4,14 @@
 
 typedef struct registro registro_t;
 
+//Estructura auxiliar para poner dentro del heap
 typedef struct dato{
 	registro_t* registro;
 	unsigned int numArchivo;
 }dato_t;
+
+/* Funcion que permite crear un registro a partir de un termino, documento y posicion */
+registro_t* registro_crear(const char*, unsigned long, unsigned long);
 
 /* Funcion que permite liberar la memoria alocada para el registro */
 void registro_destruir(registro_t*);
