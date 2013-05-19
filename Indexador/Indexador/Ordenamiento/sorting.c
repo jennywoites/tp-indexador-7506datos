bb93e066fbbc82bcba7b352ddefe54f9ff301d9d
martine9111@gmail.com <martine9111@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-05-19 02:24:46 +0000

    Correcciones al Merge y al Sort (ahora es Replacement Selection). Sin perdidas de memoria.

--- Indexador/Indexador/Ordenamiento/sorting.c
+++ Indexador/Indexador/Ordenamiento/sorting.c
@@ -9,53 +9,115 @@
 
 const char* SALIDA_TEMPORAL_SORT = "tempsort.jem";
 
+#define SORT_CANT_REGISTROS 10000
+#define SORT_CANT_RUTAS_MAX 100
 
-//POR AHORA LO HAGO CABEZA!!!!!!
-int sorting_ordenarArchivo(const char* entrada, const char* salida){
-	heap_t* heap = heap_crear(comparacionRegistros);
 
-	FILE* archEnt = fopen(entrada, lectura_archivos());
+//El metodo a utilizar es el REPLACEMENT SELECTION
+
+void sort_meterEnHeap(heap_t* heap, FILE* archEnt, unsigned long inicial){
 	unsigned long i = 0;
-	unsigned int num = 0;
-	char** rutas = malloc (sizeof(char*) * 100);
+	while ( (i+inicial) < SORT_CANT_REGISTROS && !feof(archEnt)){
+		registro_t* reg = registro_leer(archEnt);
+		dato_t* d = malloc(sizeof(dato_t));
+		d->registro = reg;
+		d->numArchivo = 0;
+		d->freezado = false;
+		if (reg != NULL)
+			heap_encolar(heap, d);
+		else
+			free(d);
+		i++;
+	}
+}
 
-	while (!feof(archEnt)){
-		emitir_impresion("Ordenando Archivo Parseado", num, 4);
-		while ( i < 100000 && !feof(archEnt)){
+void sort_guardarHastaFreezados(heap_t* heap, FILE* archEnt,FILE* archSal){
+	dato_t* anterior = NULL;
+
+	while (!heap_esta_vacio(heap) && !((dato_t*)heap_ver_max(heap))->freezado){
+
+		dato_t* actual = heap_desencolar(heap);
+		bool agregar = true;
+		if (anterior){
+			if (comparacionRegistros(anterior, actual) < 0){
+				agregar = false;
+				actual->freezado = true;
+				heap_encolar(heap, actual);
+			}
+		}
+
+		if (agregar){
+			registro_escribir(archSal, actual->registro);
+			if (anterior){
+				registro_destruir(anterior->registro);
+				free(anterior);
+			}
+			anterior = actual;
+
 			registro_t* reg = registro_leer(archEnt);
 			dato_t* d = malloc(sizeof(dato_t));
 			d->registro = reg;
 			d->numArchivo = 0;
+			d->freezado = false;
 			if (reg != NULL)
 				heap_encolar(heap, d);
 			else
 				free(d);
-			i++;
 		}
-		i = 0;
-		char* ruta = __crear_ruta(num, 999, SALIDA_TEMPORAL_SORT);
+
+	}
+	if (anterior){
+		registro_destruir(anterior->registro);
+		free(anterior);
+	}
+}
+
+void sort_desfreezar(heap_t** heap){
+	heap_t* heap_aux = heap_crear(comparacionRegistros);
+	heap_t* heap_actual = *heap;
+	while (!heap_esta_vacio(heap_actual)){
+		dato_t* dato = heap_desencolar(heap_actual);
+		if (!dato) continue;
+		dato->freezado = false;
+		heap_encolar(heap_aux, dato);
+	}
+	heap_destruir(heap_actual,NULL);
+	*heap = heap_aux;
+}
+
+
+int sorting_ordenarArchivo(const char* entrada, const char* salida){
+	heap_t* heap = heap_crear(comparacionRegistros);
+
+	FILE* archEnt = fopen(entrada, lectura_archivos());
+	unsigned int num = 0;
+	char** rutas = malloc (sizeof(char*) * SORT_CANT_RUTAS_MAX);
+
+	log_emitir("Se Comienza el proceso de Ordenamiento del archivo parcedo", LOG_ENTRADA_PROCESO);
+	while (!feof(archEnt) || !heap_esta_vacio(heap)){
+		log_emitir("Se genera particion ordenada", LOG_ENTRADA_PROCESO);
+		emitir_impresion("Ordenando Archivo Parseado", num, 4);
+		sort_meterEnHeap(heap, archEnt, heap_cantidad(heap));
+		char* ruta = __crear_ruta(num, SORT_CANT_RUTAS_MAX, SALIDA_TEMPORAL_SORT);
 		FILE* archSal = fopen(ruta, escritura_archivos());
 		rutas[num] = ruta;
 		num++;
-
-		while (!heap_esta_vacio(heap)){
-			dato_t* d = heap_desencolar(heap);
-			registro_t* reg = d->registro;
-			registro_escribir(archSal,reg);
-			registro_destruir(reg);
-			free(d);
-		}
+		sort_guardarHastaFreezados(heap, archEnt, archSal);
 		fclose(archSal);
+		sort_desfreezar(&heap);
 	}
 	fclose(archEnt);
+	int ok = merger_MergearArchivos(rutas, num,salida);
 
-	merger_MergearArchivos(rutas, num,salida);
-
+	log_emitir("Se remueven los archivos temporales del proceso de ordenamiento", LOG_ENTRADA_PROCESO);
 	for (unsigned int j = 0; j < num; j++){
 		remove(rutas[j]);
 		free(rutas[j]);
 	}
 	free(rutas);
 	heap_destruir(heap,NULL);
-	return SORTING_OK;
+	if ( ok == MERGER_OK)
+		return SORTING_OK;
+	else
+		return SORTING_ERROR;
 }
