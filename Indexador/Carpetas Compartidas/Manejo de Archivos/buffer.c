ae6e6d3a9126b5c1f747088dff4b6ca6dd750870
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-05-25 15:03:53 +0000

    buffer ya probe el escribir bit

---
+++ Indexador/Carpetas Compartidas/Manejo de Archivos/buffer.c
@@ -0,0 +1,70 @@
+#include "buffer.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+struct buffer{
+	FILE* arch_salida;
+	size_t cant_bits_almacenados;
+	Byte_t* bits_almacenados;
+};
+
+
+buffer_t* buffer_crear(FILE* fd){
+	buffer_t* buff = malloc(sizeof(buffer_t) );
+	buff->bits_almacenados = malloc(8* sizeof(Byte_t) );
+	buff->cant_bits_almacenados = 0;
+	buff->arch_salida = fd;
+
+	return buff;
+}
+
+Byte_t buffer_crear_byte(buffer_t* buff){
+	if (buff->cant_bits_almacenados!=8)
+		return 0;
+	Byte_t byte_completo = 0;
+	for (size_t i = 0; i < 8; i++){
+		byte_completo += Byte_dosElevadoALa(i) * buff->bits_almacenados[i] ;
+	}
+	return byte_completo;
+}
+
+void buffer_escribir_a_archivo(buffer_t* buff){
+	if (buff->cant_bits_almacenados!=8)
+		return ;
+	Byte_t byte_completo;
+	byte_completo = buffer_crear_byte(buff);
+	fprintf(buff->arch_salida, "%c", byte_completo );
+	buff->cant_bits_almacenados=0;
+}
+
+void buffer_escribir_bytes(buffer_t* buff, Byte_t* num, size_t cant_bytes){
+	for (size_t i=0; i<cant_bytes ;i--)
+		buffer_escribir_byte(buff, num[i] );
+}
+
+void buffer_escribir_byte(buffer_t* buff, Byte_t num){
+	size_t longitud = 8;
+	buffer_escribir_bits(buff, num, longitud);
+}
+
+void buffer_escribir_bits(buffer_t* buff, Byte_t num, size_t longitud ){
+	for (size_t i=longitud-1; i>=0 ;i--){
+		if ( num >= dosElevadoALa(i) )
+			num = num - dosElevadoALa(i);
+			buffer_escribir_bit(buff, 1);
+		buffer_escribir_bit(buff, 0);
+	}
+}
+
+void buffer_escribir_bit(buffer_t* buff, Byte_t num){
+	(buff->bits_almacenados) [ 8 - 1 - buff->cant_bits_almacenados ] = num ;
+
+	buff->cant_bits_almacenados++;
+
+	buffer_escribir_a_archivo(buff);
+}
+
+void buffer_destruir(buffer_t* buff){
+	free(buff->bits_almacenados);
+	free(buff);
+}
---
+++ Indexador/Carpetas Compartidas/Manejo de Archivos/buffer.h
@@ -0,0 +1,22 @@
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+#include <stdio.h>
+#include "../Codigos/matematicaEspecial.h"
+
+typedef struct buffer buffer_t;
+
+buffer_t* buffer_crear(FILE* fd);
+
+void buffer_escribir_bit(buffer_t* buff, Byte_t num);
+
+void buffer_escribir_bits(buffer_t* buff, Byte_t num, size_t longitud );
+
+void buffer_escribir_byte(buffer_t* buff, Byte_t num);
+
+void buffer_escribir_bytes(buffer_t* buff, Byte_t* num, size_t cant_bytes);
+
+void buffer_destruir(buffer_t* buff);
+
+
+#endif /* BUFFER_H_ */
