b1324e2b17475c717f3fecbd730f2851acf190fd
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-05-25 18:35:33 +0000

    Codificador de Unario, Binario, Gamma y Delta

---
+++ Indexador/Carpetas Compartidas/Codigos/codificador.h
@@ -0,0 +1,29 @@
+#ifndef CODIFICADOR_H_
+#define CODIFICADOR_H_
+
+#include <stdlib.h>
+#include "../Manejo de Archivos/buffer.h"
+
+#define CODIGO_UNARIO 0
+#define CODIGO_BINARIO 1
+#define CODIGO_GAMMA 2
+#define CODIGO_DELTA 3
+
+
+typedef struct codificador codificador_t;
+
+codificador_t* codificador_crear(buffer_t* buffer);
+
+void codificador_codificarUnario(codificador_t* cod, unsigned int num);
+
+void codificador_destruir(codificador_t* cod);
+
+void codificador_codificarBinario(codificador_t* cod, unsigned int num, size_t longitud);
+
+void codificador_codificarUnario(codificador_t* cod, unsigned int num);
+
+void codificador_codificarGamma(codificador_t* cod, unsigned int num);
+
+void codificador_codificarDelta(codificador_t* cod, unsigned int num);
+
+#endif /*CODIFICADOR_H_*/
