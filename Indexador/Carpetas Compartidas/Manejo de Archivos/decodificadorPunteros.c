a929b3d635fe7b4897a66ec5c01f8cfc5018de7a
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-01 19:12:22 +0000

    empezando la decod de punteros

---
+++ Indexador/Carpetas Compartidas/Manejo de Archivos/decodificadorPunteros.c
@@ -0,0 +1,6 @@
+#include "decodificadorPunteros.h"
+
+void decodificar_punteros(const char* ruta, termino_t* termino){
+	FILE* arch = fopen(ruta, "r");
+
+}
---
+++ Indexador/Carpetas Compartidas/Manejo de Archivos/decodificadorPunteros.h
@@ -0,0 +1,10 @@
+#ifndef DECODIFICADOR_PUNTEROS_H_
+#define DECODIFICADOR_PUNTEROS_H_
+
+#include <stdio.h>
+#include "termino.h"
+
+void decodificar_punteros(const char* FILE, termino_t* termino);
+
+
+#endif /* DECODIFICADOR_PUNTEROS_H_ */
