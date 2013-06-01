809713975d33f1fe5496d08bdccad5ffb9d4edb1
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-01 21:11:32 +0000

    Decodificador de Punteros: Traduce lo que haya en el archivo a partir del offset y la cantidad de documentos (frecuencia) que tenga un termino. Lo primero es la cantidad de posiciones en Delta. Lo segundo: listas de posiciones en delta como listas de distancias

--- trunk/Indexador/Carpetas Compartidas/Manejo de Archivos/decodificadorPunteros.h
+++ trunk/Indexador/Carpetas Compartidas/Manejo de Archivos/decodificadorPunteros.h
@@ -3,8 +3,12 @@
 
 #include <stdio.h>
 #include "termino.h"
+#include "../TDAs/lista.h"
 
-void decodificar_punteros(const char* FILE, termino_t* termino);
+/* Si no logro abrir el archivo o la frecuencia del termino es 0, devuelve NULL
+ * MEMORIA: Se pide memoria para cada nodo en la lista, cuando se destruya la lista
+ * pasarle función "free" */
+lista_t* decodificar_punteros(const char* FILE, termino_t* termino);
 
 
 #endif /* DECODIFICADOR_PUNTEROS_H_ */
