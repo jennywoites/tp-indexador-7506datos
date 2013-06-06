a518b28accf47efb29f8f42d24dfec21c3289e1a
martine9111@gmail.com <martine9111@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-06 19:20:42 +0000

    Refactorizacion: ahora se definen los codigos a usar en un archivo aparte, y el registro/termino escribe/lee a partir de funciones mas genericas, que no dependen de como se comprimen (facilitará modificaciones)

---
+++ Indexador/Carpetas Compartidas/Codigos/trasbordoCodigo.h
@@ -0,0 +1,40 @@
+#ifndef TRASBORDOCODIGO_H_
+#define TRASBORDOCODIGO_H_
+#include "../Manejo de Archivos/buffer.h"
+#include "../Manejo de Archivos/debuffer.h"
+#include "codificador.h"
+#include "decodificador.h"
+
+/* COMPRESION */
+void comprimir_FrecuenciaDocumentos(buffer_t* cod, unsigned int num);
+
+void comprimir_FrecuenciaPosiciones(buffer_t* cod, unsigned int num);
+
+void comprimir_IndiceDistanciaDocumentos(buffer_t* cod, unsigned int num);
+
+void comprimir_IndiceDistanciaPosiciones(buffer_t* cod, unsigned int num);
+
+void comprimir_LexicoRepetidos(buffer_t* cod, unsigned int num);
+
+void comprimir_LexicoDiferentes(buffer_t* cod, unsigned int num);
+
+void comprimir_LexicoOffset(buffer_t* cod, unsigned int num);
+
+
+/* DESCOMPRESION */
+unsigned int descomprimir_FrecuenciaDocumentos(debuffer_t* cod);
+
+unsigned int descomprimir_FrecuenciaPosiciones(debuffer_t* cod);
+
+unsigned int descomprimir_IndiceDistanciaDocumentos(debuffer_t* cod);
+
+unsigned int descomprimir_IndiceDistanciaPosiciones(debuffer_t* cod);
+
+unsigned int descomprimir_LexicoRepetidos(debuffer_t* cod);
+
+unsigned int descomprimir_LexicoDiferentes(debuffer_t* cod);
+
+unsigned int descomprimir_LexicoOffset(debuffer_t* cod);
+
+
+#endif /* TRASBORDOCODIGO_H_ */
