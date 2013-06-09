57d6d5af1960c602a447ab48024991eca204ba40
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-09 03:33:10 +0000

    Agrego la funcion de codificar binario prefijo  codigos de golomb

--- Indexador/Carpetas Compartidas/Codigos/codificador.c
+++ Indexador/Carpetas Compartidas/Codigos/codificador.c
@@ -2,6 +2,13 @@
 #include <stddef.h>
 #include "matematicaEspecial.h"
 #include <stdio.h>
+#include "../TDAs/abb.h"
+#include "../TDAs/arbolHuff.h"
+#include "../TDAs/lista.h"
+#include <string.h>
+#include "../TDAs/heap.h"
+
+#define CANT_CHARS 3
 
 struct codificador{
 	buffer_t* buffer;
@@ -67,6 +74,49 @@ void codificador_codificarDelta(buffer_t* cod, unsigned int num){
 	codificador_codificarGamma(cod, num_gamma);
 	codificador_codificarBinario(cod, num_binario, cant_bits_binario);
 }
+
+int* comparacion_prefijo(const char* a, const char* b){
+	unsigned int numIzq = atoi(a);
+	unsigned int numDer = atoi(b);
+	return b-a;
+}
+
+void codificador_codificarBinarioPrefijo(buffer_t* buffer, unsigned int num, unsigned int b){
+
+	arbol_huff_t* arbol = arbol_huff_crear((cmp_func_t)comparacion_prefijo, b);
+
+	if(!arbol)
+		return;
+	
+	lista_t* lista_bits = arbol_huff_obtener_lista_bits(arbol,num);
+	
+	while(!lista_esta_vacia(lista_bits)){
+		Byte_t* numero = lista_borrar_primero(lista_bits);
+		buffer_escribir_bit(buffer, *numero);
+		if (*numero == 0)
+			printf("imprime: 0 \n" );
+		if (*numero == 1)
+			printf("imprime: 1 \n" );
+		free(numero);
+	}
+	
+	lista_destruir(lista_bits, free);
+	
+	arbol_huff_destruir(arbol);
+
+}
+
+void codificador_codificarGolomb(buffer_t* buffer, unsigned int num, unsigned int b){
+	if ((b == 0) || (num == 0))
+		return;
+	
+	unsigned int q = (num - 1) / b;
+	unsigned int r = num - (q * b) - 1;
+	
+	codificador_codificarUnario(buffer, q + 1);
+	codificador_codificarBinarioPrefijo(buffer, r, b);
+}
+
 
 void codificador_destruir(codificador_t* cod){
 	free(cod);
