1742b5520e93275feb6f59915ff29b244ce3e6ab
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-09 04:51:21 +0000

    a ver si decodificamos

--- Indexador/Carpetas Compartidas/Codigos/decodificador.h
+++ Indexador/Carpetas Compartidas/Codigos/decodificador.h
@@ -12,14 +12,10 @@ typedef struct decodificador decodificador_t;
 decodificador_t* decodificador_crear(debuffer_t* debuffer);
 void decodificador_destruir(decodificador_t* decodificador);
 
-//unsigned int decodificador_decodificarUnario(decodificador_t* decodificador);
-//unsigned int decodificador_decodificarBinario(decodificador_t* decodificador, size_t longitud);
-//unsigned int decodificador_decodificarGamma(decodificador_t* decodificador);
-//unsigned int decodificador_decodificarDelta(decodificador_t* decodificador);
-
 unsigned int decodificador_decodificarUnario(debuffer_t* decodificador);
 unsigned int decodificador_decodificarBinario(debuffer_t* decodificador, size_t longitud);
 unsigned int decodificador_decodificarGamma(debuffer_t* decodificador);
 unsigned int decodificador_decodificarDelta(debuffer_t* decodificador);
+unsigned int decodificador_decodificarGolomb(debuffer_t* debuffer, size_t b);
 
 #endif /*DECODIFICADOR_H_*/
