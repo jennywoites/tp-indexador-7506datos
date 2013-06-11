9b70a0521993305a04c6aa6312141fb9e07e7114
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-11 03:06:27 +0000

    distancias de documentos en Golomb. B de golomb en gamma

--- Indexador/Carpetas Compartidas/Codigos/trasbordoCodigo.c
+++ Indexador/Carpetas Compartidas/Codigos/trasbordoCodigo.c
@@ -9,8 +9,9 @@ void comprimir_FrecuenciaPosiciones(buffer_t* cod, unsigned int num){
 	codificador_codificarDelta(cod,num);
 }
 
-void comprimir_IndiceDistanciaDocumentos(buffer_t* cod, unsigned int num){
-	codificador_codificarGamma(cod,num);
+void comprimir_IndiceDistanciaDocumentos(buffer_t* cod, unsigned int num, size_t b){
+	//codificador_codificarGamma(cod,num);
+	codificador_codificarGolomb(cod, num, b);
 }
 
 void comprimir_IndiceDistanciaPosiciones(buffer_t* cod, unsigned int num){
@@ -37,8 +38,9 @@ unsigned int descomprimir_FrecuenciaPosiciones(debuffer_t* cod){
 	return decodificador_decodificarDelta(cod);
 }
 
-unsigned int descomprimir_IndiceDistanciaDocumentos(debuffer_t* cod){
-	return decodificador_decodificarGamma(cod);
+unsigned int descomprimir_IndiceDistanciaDocumentos(debuffer_t* cod, size_t b){
+	//return decodificador_decodificarGamma(cod);
+	return decodificador_decodificarGolomb(cod, b);
 }
 
 unsigned int descomprimir_IndiceDistanciaPosiciones(debuffer_t* cod){
@@ -55,4 +57,12 @@ unsigned int descomprimir_LexicoDiferentes(debuffer_t* cod){
 
 unsigned int descomprimir_LexicoOffset(debuffer_t* cod){
 	return decodificador_decodificarDelta(cod);
+}
+
+void comprimir_BGolomb(buffer_t* cod, unsigned int num){
+	codificador_codificarGamma(cod, num);
+}
+
+unsigned int descomprimir_BGolomb(debuffer_t* cod){
+	return decodificador_decodificarGamma(cod);
 }
