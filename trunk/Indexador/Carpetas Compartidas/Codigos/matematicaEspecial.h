e1609cca811ebb0d2c16c7f68705d64374a72a2a
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-11 03:04:35 +0000

    quisieramos calcular el b optimo

--- trunk/Indexador/Carpetas Compartidas/Codigos/matematicaEspecial.h
+++ trunk/Indexador/Carpetas Compartidas/Codigos/matematicaEspecial.h
@@ -4,11 +4,13 @@
 /* DEFINICION ESCENCIAL*/
 #define Byte_t unsigned char
 #define BITS_X_BYTE 8
-
+#include <stddef.h>
 Byte_t Byte_dosElevadoALa(unsigned int);
 
 unsigned int dosElevadoALa(unsigned int);
 
 unsigned int maxPot2(unsigned int);
+
+size_t calcular_B_optimo(float p);
 
 #endif /* MATEMATICAESPECIAL_H_ */
