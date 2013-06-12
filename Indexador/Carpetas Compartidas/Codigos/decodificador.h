64f211be98f7c96d9b241a10f3c87cbd88209a0e
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-12 03:22:57 +0000

    se recibe el arbol de huff

--- Indexador/Carpetas Compartidas/Codigos/decodificador.h
+++ Indexador/Carpetas Compartidas/Codigos/decodificador.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include "../Manejo de Archivos/debuffer.h"
+#include "../TDAs/arbolHuff.h"
 
 #define NO_NUMERO 0
 
@@ -16,6 +17,6 @@ unsigned int decodificador_decodificarUnario(debuffer_t* decodificador);
 unsigned int decodificador_decodificarBinario(debuffer_t* decodificador, size_t longitud);
 unsigned int decodificador_decodificarGamma(debuffer_t* decodificador);
 unsigned int decodificador_decodificarDelta(debuffer_t* decodificador);
-unsigned int decodificador_decodificarGolomb(debuffer_t* debuffer, size_t b);
+unsigned int decodificador_decodificarGolomb(debuffer_t* debuffer, size_t b, arbol_huff_t* arbol);
 
 #endif /*DECODIFICADOR_H_*/
