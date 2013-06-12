8d14a24a82992c768285c79c1bf592410a5e9505
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-12 03:21:34 +0000

    se indexa la cantidad de terminos en un documento

--- trunk/Indexador/Indexador/Indexado/indexer.h
+++ trunk/Indexador/Indexador/Indexado/indexer.h
@@ -10,7 +10,7 @@
  * El lexico se guardara en el archivo de ruta destino_lexico, y los punteros destino_index
  * Si salio bien el indexado, devuelvo INDEXER_OK, sino INDEXER_ERROR
  * Memoria: No guarda nada, solo crea los archivos correspondientes */
-int indexer_indexar(size_t cant_documentos,const char* origen, const char* destino_index, const char* destino_lexico_frontCoding, const char* destino_lexico_diferentes);
+int indexer_indexar(size_t cant_documentos,const char* origen, const char* destino_index, const char* destino_lexico_frontCoding, const char* destino_lexico_diferentes, const char* ruta_tams);
 
 
 
