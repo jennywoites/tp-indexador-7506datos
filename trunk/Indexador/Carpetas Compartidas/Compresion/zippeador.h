e271918fc1dab639784f998cbe7b5628fc53e68a
martine9111@gmail.com <martine9111@gmail.com@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-13 18:34:03 +0000

    Compresion de archivitos ;)

---
+++ trunk/Indexador/Carpetas Compartidas/Compresion/zippeador.h
@@ -0,0 +1,10 @@
+#ifndef ZIPPEADOR_H_
+#define ZIPPEADOR_H_
+
+void zippeador_comprimir(const char* org, const char* dst);
+
+void zippeador_descomprimir(const char* org, const char* dst);
+
+
+
+#endif /* ZIPPEADOR_H_ */
