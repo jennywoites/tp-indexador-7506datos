60f78af421b2f5244feea7f42bdb0c7393f5dcd2
egenender <egenender@e658cecd-d204-55e6-7a05-578c1eef36c5>
2013-06-14 03:38:30 +0000

    me gusta hacer pruebas para ver si las cosas andan

---
+++ trunk/Indexador/Carpetas Compartidas/Manejo de Archivos/Pruebas/pruebaF.c
@@ -0,0 +1,19 @@
+#include "../funcionesGeneralesArchivos.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+	char * directorio = malloc(sizeof(char)*10);
+	strcpy(directorio, "hola");
+	
+	char* resul = __crear_ruta_repo(directorio, "parser.jem");
+	
+	printf("this: %s \n", resul );
+	
+	free(directorio);
+	free(resul);
+	
+	return 0;
+}
